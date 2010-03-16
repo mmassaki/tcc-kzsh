487752c66419d2e378b40ba07782cea8d9ab2b59
henrique.zambon <henrique.zambon@edb4005a-225f-11df-88ee-e13ad67854f8>
2010-03-16 01:17:15 +0000

    - correcoes no documento de requisitos funcionais - criacao do documento de especificacao de requisitos

---
+++ trunk/TCC-core.mm
@@ -0,0 +1,37 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1267736670007" ID="Freemind_Link_710676433" MODIFIED="1267740355935" TEXT="TCC">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1267736735795" ID="Freemind_Link_1036504195" MODIFIED="1268685692591" POSITION="right" TEXT="Navega&#xe7;&#xe3;o" VSHIFT="4">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1267736793960" HGAP="38" ID="Freemind_Link_807539784" MODIFIED="1268685707180" TEXT="Tr&#xe2;nsito" VSHIFT="-22">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1267737503170" ID="Freemind_Link_1640067072" MODIFIED="1267792208703" TEXT="Usu&#xe1;rio">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1268685644899" ID="Freemind_Link_1592768429" MODIFIED="1268685647882" TEXT="CET">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1267736819328" HGAP="36" ID="Freemind_Link_1575480631" MODIFIED="1268685700336" TEXT="Mapeamento real-time" VSHIFT="10">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267737332704" HGAP="32" ID="Freemind_Link_678991819" MODIFIED="1268685710102" TEXT="Localiza&#xe7;&#xe3;o atual" VSHIFT="24">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1267737514233" HGAP="22" ID="Freemind_Link_1006379040" MODIFIED="1268685686613" POSITION="left" TEXT="Servidor" VSHIFT="-73">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1267737516441" ID="Freemind_Link_1440839103" MODIFIED="1267740355964" TEXT="Estat&#xed;sticas">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267737541376" ID="_" MODIFIED="1267740355964" TEXT="Banco de dados">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1267739729763" HGAP="54" ID="Freemind_Link_324819295" MODIFIED="1267893405761" POSITION="left" TEXT="Mapas" VSHIFT="33">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
