a4e77ae54e419d884427cdbfad7a24e9c5089edd
andre.kido <andre.kido@edb4005a-225f-11df-88ee-e13ad67854f8>
2010-09-29 21:38:31 +0000

    lista links map

---
+++ diagrams/TCC-Core4.mm
@@ -0,0 +1,100 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1267736670007" ID="Freemind_Link_710676433" MODIFIED="1267740355935" TEXT="TCC">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1267736735795" ID="Freemind_Link_1036504195" MODIFIED="1285789569912" POSITION="right" TEXT="Navega&#xe7;&#xe3;o" VSHIFT="-2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1267736741626" ID="Freemind_Link_740846590" MODIFIED="1285789569913" TEXT="Rotas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1267739580464" ID="Freemind_Link_967188614" MODIFIED="1285792280158" TEXT="Favoritos">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267739821952" ID="Freemind_Link_1148849754" MODIFIED="1285792465337" TEXT="Navega&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1267736793960" ID="Freemind_Link_807539784" MODIFIED="1285789569915" TEXT="Tr&#xe2;nsito">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267736819328" ID="Freemind_Link_1575480631" MODIFIED="1285789569917" TEXT="Mapeamento em tempo real">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1267737048826" ID="Freemind_Link_876604940" MODIFIED="1267740355942" TEXT="Cores">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267737051914" ID="Freemind_Link_213990727" MODIFIED="1267740355943" TEXT="&#xcd;cones">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1267736911765" ID="Freemind_Link_1085564772" MODIFIED="1285789569919" TEXT="Alertas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1267736915764" ID="Freemind_Link_1621926063" MODIFIED="1267740355944" TEXT="Sem&#xe1;foro quebrado">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267736921380" ID="Freemind_Link_1872443930" MODIFIED="1267740355944" TEXT="Alagamento">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267736933603" ID="Freemind_Link_669131967" MODIFIED="1267740355945" TEXT="Acidente">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267736941227" ID="Freemind_Link_964319791" MODIFIED="1267740355945" TEXT="Via interditada">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1268676958274" ID="Freemind_Link_1101024661" MODIFIED="1268676961766" TEXT="Obst&#xe1;culos">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1267738666088" ID="Freemind_Link_643849695" MODIFIED="1278626329885" TEXT="Radar">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1278626333989" ID="Freemind_Link_1414931844" MODIFIED="1278626339243" TEXT="Blitz">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1267737332704" ID="Freemind_Link_678991819" MODIFIED="1285789569947" TEXT="Localiza&#xe7;&#xe3;o atual">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1267738335900" HGAP="44" ID="Freemind_Link_69390715" MODIFIED="1285789569949" POSITION="right" TEXT="Mapas" VSHIFT="-73">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1267739296282" HGAP="58" ID="Freemind_Link_1898683592" MODIFIED="1285789569950" POSITION="left" TEXT="Integra&#xe7;&#xe3;o" VSHIFT="-21">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1268675464598" ID="Freemind_Link_649287669" MODIFIED="1285792420803" TEXT="Twitter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267745206057" ID="ID_1905667601" MODIFIED="1285792424659" TEXT="Facebook">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267745209104" ID="ID_674322958" MODIFIED="1285792399517" TEXT="SMS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267745210464" ID="ID_433410170" MODIFIED="1285792399526" TEXT="E-mail">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1267737514233" HGAP="58" ID="Freemind_Link_1006379040" MODIFIED="1285789569955" POSITION="left" TEXT="Servidor" VSHIFT="-4">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1267737516441" ID="Freemind_Link_1440839103" MODIFIED="1285789569957" TEXT="Estat&#xed;sticas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1267737541376" ID="_" MODIFIED="1285789569959" TEXT="Banco de dados">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
